db45a1e749ef281edcb17a55d44e129b47378320
Aevmanya <78971052+Aevmanya@users.noreply.github.com>
2026-09-23 18:28:45 +0530

    Add safe duplicate email query

--- src/main/java/com/GiveGrid/store/repository/UserRepository.java
+++ src/main/java/com/GiveGrid/store/repository/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
     boolean existsByEmailAndIdNot(String email, Long id);
+
+    boolean existsByEmailAndIdNot(String email, Long id);
 }
