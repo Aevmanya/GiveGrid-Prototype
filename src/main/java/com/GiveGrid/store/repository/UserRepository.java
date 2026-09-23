abacca65e7f729e908f05f648034037d05fc6452
Aevmanya <78971052+Aevmanya@users.noreply.github.com>
2026-09-23 18:30:27 +0530

    Fix duplicate email query declaration

--- src/main/java/com/GiveGrid/store/repository/UserRepository.java
+++ src/main/java/com/GiveGrid/store/repository/UserRepository.java
@@ -12,6 +12,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
     boolean existsByEmailAndIdNot(String email, Long id);
-
-    boolean existsByEmailAndIdNot(String email, Long id);
 }
