a33b606ba0f22e772da34a7d23b6deea45eca932
Aevmanya <78971052+Aevmanya@users.noreply.github.com>
2026-09-24 06:23:38 +0530

    Use accepted donations for recorded activity

--- src/main/java/com/GiveGrid/store/controller/ProfileController.java
+++ src/main/java/com/GiveGrid/store/controller/ProfileController.java
@@ -1,7 +1,6 @@
 package com.GiveGrid.store.controller;
 
 import com.GiveGrid.store.entity.User;
-import com.GiveGrid.store.repository.DonationRepository;
 import com.GiveGrid.store.service.PendingDonationService;
 import com.GiveGrid.store.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +9,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
 
 @Controller
 public class ProfileController {
 
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private DonationRepository donationRepository;
 
     @Autowired
     private PendingDonationService pendingDonationService;
@@ -32,7 +27,8 @@ public class ProfileController {
 
         model.addAttribute("user", user);
 
-        List<com.GiveGrid.store.entity.Donation> allDonations = donationRepository.findByUserOrderByDonatedAtDesc(user);
+        java.util.List<com.GiveGrid.store.entity.PendingDonation> allDonations =
+                pendingDonationService.getAcceptedForBuyer(user);
         model.addAttribute("donationCount", allDonations.size());
         model.addAttribute("donations", allDonations.stream().limit(3).toList());
         model.addAttribute("requestedDonations", pendingDonationService.getForBuyer(user));
